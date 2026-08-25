b5390f82158e52e9bc2758ad9d1e88f3bd48c746
Jerry Jung <wisestonster@gmail.com>
2026-08-25 07:06:28 +0000

    Add '솔루션 보기' button to the left of '프로젝트 보기' in the home hero

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,9 @@ export default async function Home() {
               블록체인 비즈니스 혁신을 주도합니다
             </p>
             <div className="hero-actions">
+              <Link className="button button--secondary" href="/product">
+                솔루션 보기
+              </Link>
               <Link className="button" href="/project">
                 프로젝트 보기 <Arrow />
               </Link>
